ec558dd9e3b849ac488c37c7ab0728b83734e34c
jonathan <jomartinezch@gmail.com>
2017-05-28 20:55:17 -0500

    fixed collision and overlap

---
+++ README.md
@@ -0,0 +1,12 @@
+# dUNgeon
+
+### About
+
+A game made using phaser.js library
+
+### How to?
+
+Assets:
+
+Roguelike /tilemaps from [Kenney](http://kenney.nl/).
+Other /sprites from [Quale](http://quale-art.blogspot.com.co/p/addventure-mini-figs.html)
--- src/src.js
+++ src/src.js
@@ -4,11 +4,11 @@ var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game', { preload: preload, c
 var map;
 var layer;
 var cursors;
-var player;
+var player, centaur;
 
 var dialogue;
 var dialogues = {
-    1: "Hey Dad, I'mthirsty. Canyou get mesome milk?-baby Seb",
+    1: "Hey Dad, I'm thirsty. Canyou get mesome milk?",
     2: "The key isin the shed.-Mum",
     3: "Here lies theFairy Queen.She will neverbe forgotten",
     4: "What are thosedark patchesin the water?",
@@ -32,6 +32,9 @@ function preload() {
     game.load.image('roguelike_character', 'assets/tilemaps/roguelike_character.png');
 
     game.load.spritesheet('player', 'assets/sprites/spaceman.png', 16, 16);
+
+    game.load.spritesheet('centaur', 'assets/sprites/centaur.png');
+
     game.load.tilemap('tilemap', 'assets/tilemaps/un_map.json', null, Phaser.Tilemap.TILED_JSON);
 
     this.game.load.json('speech', 'assets/speechs/test.json');
@@ -40,56 +43,57 @@ function preload() {
 
 function create() {
 
-    map = game.add.tilemap('tilemap');
+  map = game.add.tilemap('tilemap');
 
-    //  Now add in the tileset
-    // addTilesetImage(tileset, key, tileWidth, tileHeight, tileMargin, tileSpacing, gid)
-    map.addTilesetImage('roguelike_sheet');
-    map.addTilesetImage('roguelike_indoor');
-    map.addTilesetImage('roguelike_dungeon');
-    map.addTilesetImage('roguelike_city');
-    map.addTilesetImage('roguelike_character');
+  //  Now add in the tileset
+  // addTilesetImage(tileset, key, tileWidth, tileHeight, tileMargin, tileSpacing, gid)
+  map.addTilesetImage('roguelike_sheet');
+  map.addTilesetImage('roguelike_indoor');
+  map.addTilesetImage('roguelike_dungeon');
+  map.addTilesetImage('roguelike_city');
+  map.addTilesetImage('roguelike_character');
 
-    //  Create our layer
-    layer = map.createLayer('Ground');
-    layer = map.createLayer('Ground 2');
-    layer = map.createLayer('Buildings');
-    layer = map.createLayer('Roofs');
-    layer = map.createLayer('Extra');
+  //  Create our layers
 
-    //  Resize the world
-    layer.resizeWorld();
+  ground_layer = map.createLayer('Ground');
+  ground2_layer = map.createLayer('Ground 2');
+  building_layer = map.createLayer('Buildings');
+  roof_layer = map.createLayer('Roofs');
+  extra_layer = map.createLayer('Extra');
+  character_layer = map.createLayer('Characters');
 
-    //  This isn't totally accurate, but it'll do for now
-    map.setCollisionBetween(54, 83);
+  //  Resize the world
+  ground_layer.resizeWorld();
+  ground2_layer.resizeWorld();
+  building_layer.resizeWorld();
+  roof_layer.resizeWorld();
+  character_layer.resizeWorld();
 
-    //  Un-comment this on to see the collision tiles
-    // layer.debug = true;
+  //  This isn't totally accurate, but it'll do for now
+  map.setCollisionBetween(1, 3500, true, building_layer);
+  map.setCollisionBetween(1, 3500, true, roof_layer);
 
-    //  Player
-    player = game.add.sprite(48, 48, 'player', 1);
-    player.animations.add('left', [8,9], 10, true);
-    player.animations.add('right', [1,2], 10, true);
-    player.animations.add('up', [11,12,13], 10, true);
-    player.animations.add('down', [4,5,6], 10, true);
+  // set aditional characters
+  centaur = game.add.sprite(100, 50, 'centaur');
+  game.physics.enable(centaur, Phaser.Physics.ARCADE);
 
-    game.physics.enable(player, Phaser.Physics.ARCADE);
+  //  set player
+  player = game.add.sprite(100, 50, 'player', 1);
+  player.animations.add('left', [8,9], 10, true);
+  player.animations.add('right', [1,2], 10, true);
+  player.animations.add('up', [11,12,13], 10, true);
+  player.animations.add('down', [4,5,6], 10, true);
 
-    player.body.setSize(10, 14, 2, 1);
+  game.physics.enable(player, Phaser.Physics.ARCADE);
+  player.body.setSize(10, 14, 2, 1);
+  game.camera.follow(player);
 
-    game.camera.follow(player);
+  cursors = game.input.keyboard.createCursorKeys();
+  dialogue = document.getElementById("dialogue");
 
-    cursors = game.input.keyboard.createCursorKeys();
-
-    var help = game.add.text(16, 16, 'Arrows to move', { font: '14px Arial', fill: '#ffffff' });
-    help.fixedToCamera = true;
-
-    dialogue = document.getElementById("dialogue");
 }
 
 function update() {
-
-    game.physics.arcade.collide(player, layer);
 
     player.body.velocity.set(0);
 
@@ -112,12 +116,16 @@ function update() {
     {
         player.body.velocity.y = 100;
         player.play('down');
-        showDialogue(1);
     }
     else
     {
         player.animations.stop();
     }
+
+    //game.physics.arcade.collide(player, non_walkable_group);
+    game.physics.arcade.overlap(player, centaur, function() { showDialogue("Soy el guardian de zootecnia, que quieres?"); }, null, this);
+    game.physics.arcade.collide(player, building_layer);
+    game.physics.arcade.collide(player, roof_layer);
 
 }
 
@@ -127,9 +135,14 @@ function render() {
 
 }
 
-function showDialogue(story_index){
-  dialogue.style.visibility = 'visible';
-  dialogue.childNodes[1].innerText = dialogues[ story_index ];
+function showDialogue(message, timeout){
 
-  setTimeout(function(){ dialogue.style.visibility = 'hidden'; }, 4000);
+  dialogue.style.visibility = 'visible';
+  dialogue.childNodes[1].innerText = message;
+
+  if(typeof obj == 'undefined'){
+    timeout = 1500;
+  }
+
+  setTimeout(function(){ dialogue.style.visibility = 'hidden'; }, timeout);
 }
